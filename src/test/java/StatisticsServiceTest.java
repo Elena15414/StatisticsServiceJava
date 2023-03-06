4c790ef171a0483b00f855c1dcfb45ddbbc4a848
Elena Datsenko <Elenasheglova15414@Gmail.com>
2023-03-07 00:51:42 +0300

    Readme

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -1,8 +1,7 @@
-package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+import ru.netology.statistic.StatisticsService;
 
 public class StatisticsServiceTest {
 
@@ -17,4 +16,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
